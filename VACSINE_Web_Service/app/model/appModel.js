33db1d20c147bad63c9f83f092b572a2d697ae0d
Estian <estianyssel@gmail.com>
2019-10-21 19:42:27 +0200

    Created endpoint for persons post.subscribe(

--- VACSINE_Web_Service/app/model/appModel.js
+++ VACSINE_Web_Service/app/model/appModel.js
@@ -2,101 +2,115 @@
 var sql = require('../db.js');
 
 //User object constructor
-var User = function(user){
+var User = function (user) {
     //this.user = user.user;
     this.first_name = user.first_name;
     this.last_name = user.last_name;
     this.email = user.email;
 };
 
-User.createUser = function (newUser, result) {    
-        sql.query("INSERT INTO user set ?", newUser, function (err, res) {
-                
-                if(err) {
-                    console.log("error: ", err);
-                    result(err, null);
-                }
-                else{
-                    console.log(res.insertId);
-                    result(null, res.insertId);
-                }
-            });           
+User.createUser = function (newUser, result) {
+    sql.query("INSERT INTO user set ?", newUser, function (err, res) {
+
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+        else {
+            console.log(res.insertId);
+            result(null, res.insertId);
+        }
+    });
 };
 
+User.createPerson = function (name, surname, user_id, site_id, result) {
+    sql.query("INSERT INTO person(name, surname, user_id, site_id) VALUES (?, ?, ?, ?)", [name, surname, user_id, site_id], function (err, res) {
+        if(err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+        else {
+            console.log("Person inserted");
+        }
+    })
+}
+
 User.getUserById = function (user_id, result) {
-        sql.query("Select * from user where user_id = ? ", user_id, function (err, res) {             
-                if(err) {
-                    console.log("error: ", err);
-                    result(err, null);
-                }
-                else{
-                    result(null, res);
-              
-                }
-            });   
+    sql.query("Select * from user where user_id = ? ", user_id, function (err, res) {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+        }
+        else {
+            result(null, res);
+
+        }
+    });
 };
 
 User.getAllUser = function (result) {
-        sql.query("Select * from user", function (err, res) {
+    sql.query("Select * from user", function (err, res) {
 
-                if(err) {
-                    console.log("error: ", err);
-                    result(null, err);
-                }
-                else{
-                  console.log('users : ', res);  
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        }
+        else {
+            console.log('users : ', res);
 
-                 result(null, res);
-                }
-            });   
+            result(null, res);
+        }
+    });
 };
 
-User.updateById = function(id, user, result){
-  sql.query("UPDATE user SET user = ? WHERE id = ?", [user.user, id], function (err, res) {
-          if(err) {
-              console.log("error: ", err);
-                result(null, err);
-             }
-           else{   
-             result(null, res);
-                }
-            }); 
+User.updateById = function (id, user, result) {
+    sql.query("UPDATE user SET user = ? WHERE id = ?", [user.user, id], function (err, res) {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        }
+        else {
+            result(null, res);
+        }
+    });
 };
 
-User.remove = function(id, result){
-     sql.query("DELETE FROM user WHERE user_id = ?", [id], function (err, res) {
 
-                if(err) {
-                    console.log("error: ", err);
-                    result(null, err);
-                }
-                else{
-               
-                 result(null, res);
-                }
-            }); 
+
+User.remove = function (id, result) {
+    sql.query("DELETE FROM user WHERE user_id = ?", [id], function (err, res) {
+
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        }
+        else {
+
+            result(null, res);
+        }
+    });
 };
 
 User.getAllPersons = function (result) {
     sql.query("SELECT person_id FROM PERSON", function (err, res) {
 
-            if(err) {
-                console.log("error: ", err);
-                result(null, err);
-            }
-            else{
-              console.log('users : ', res);  
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        }
+        else {
+            console.log('users : ', res);
 
-             result(null, res);
-            }
-        });   
+            result(null, res);
+        }
+    });
 };
 
 User.updateStatus = function (siteId, personId, result) {
     console.log('PersonId = ' + personId)
     console.log('SiteId = ' + siteId)
-    sql.query("UPDATE status SET person_identified = ? WHERE site_id = ?", [personId, siteId],  function (err, res) {
-        if(err) {
+    sql.query("UPDATE status SET person_identified = ? WHERE site_id = ?", [personId, siteId], function (err, res) {
+        if (err) {
             console.log("error: ", err);
             result(null, err);
         }
@@ -108,11 +122,11 @@ User.updateStatus = function (siteId, personId, result) {
 
 User.getStatus = function (siteId, result) {
     sql.query("SELECT * FROM status WHERE site_id = ?", siteId, function (err, res) {
-        if(err) {
+        if (err) {
             console.log("error: ", err);
             result(err, null);
         }
-        else{
+        else {
             result(null, res);
         }
     })
@@ -121,8 +135,8 @@ User.getStatus = function (siteId, result) {
 User.allowEntry = function (siteId, result) {
     //console.log('PersonId = ' + personId)
     console.log('SiteId = ' + siteId)
-    sql.query("UPDATE status SET allow_entry = 1 WHERE site_id = ?", siteId,  function (err, res) {
-        if(err) {
+    sql.query("UPDATE status SET allow_entry = 1 WHERE site_id = ?", siteId, function (err, res) {
+        if (err) {
             console.log("error: ", err);
             result(null, err);
         }
@@ -135,8 +149,8 @@ User.allowEntry = function (siteId, result) {
 User.disallowEntry = function (siteId, result) {
     //console.log('PersonId = ' + personId)
     console.log('SiteId = ' + siteId)
-    sql.query("UPDATE status SET allow_entry = 0 WHERE site_id = ?", siteId,  function (err, res) {
-        if(err) {
+    sql.query("UPDATE status SET allow_entry = 0 WHERE site_id = ?", siteId, function (err, res) {
+        if (err) {
             console.log("error: ", err);
             result(null, err);
         }
@@ -149,8 +163,8 @@ User.disallowEntry = function (siteId, result) {
 User.capturePerson = function (siteId, personId, result) {
     console.log('PersonId = ' + personId)
     console.log('SiteId = ' + siteId)
-    sql.query("UPDATE status SET capture_person = ? WHERE site_id = ?", [personId, siteId],  function (err, res) {
-        if(err) {
+    sql.query("UPDATE status SET capture_person = ? WHERE site_id = ?", [personId, siteId], function (err, res) {
+        if (err) {
             console.log("error: ", err);
             result(null, err);
         }
@@ -161,4 +175,4 @@ User.capturePerson = function (siteId, personId, result) {
 }
 
 
-module.exports= User;
+module.exports = User;
--- VACSINE_Web_Service/app/routes/appRoutes.js
+++ VACSINE_Web_Service/app/routes/appRoutes.js
@@ -14,6 +14,11 @@ module.exports = function(app) {
   
   app.route('/persons')
     .get(todoList.list_all_persons)
+    .post(todoList.create_a_person)
+
+  app.route('/persons/:personId')
+    //.put(todoList.update_a_person)
+    //.delete(todoList.delete_a_person)
 
   app.route('/status/:siteId')
     .put(todoList.update_status)
